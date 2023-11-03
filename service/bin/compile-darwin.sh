89436373665d27edaf076ad7dbf38fc0f2096fcc
Adrian Rudnik <adrian@klonmaschine.de>
2023-11-03 14:14:46 +0100

    Added dedicated darwin arm64/amd64 compile script

---
+++ service/bin/compile-darwin.sh
@@ -0,0 +1,113 @@
+#!/bin/zsh -ex
+
+# Requirements:
+# brew install imagemagick
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  APP_VERSION=$1
+else
+  echo 'Please provide numeric semver.'
+  exit 1
+fi
+
+if [[ $(uname -m) != 'arm64' ]]; then
+  echo "Use the MAC you bought."
+  exit 1
+fi
+
+mkdir -p dist/{deploy,os/darwin}
+
+# build AMD64
+CGO_ENABLED=1 \
+GOARCH=amd64 \
+GOOS=darwin \
+  go build -ldflags '-s' -o Ablegram_darwin_amd64 .
+
+# build ARM64
+CGO_ENABLED=1 \
+GOARCH=arm64 \
+GOOS=darwin \
+  go build -ldflags '-s' -o Ablegram_darwin_arm64 .
+
+# merge to universal binary
+lipo \
+  -create Ablegram_darwin_amd64 Ablegram_darwin_arm64 \
+  -o Ablegram_darwin_universal
+
+# review the universal binary
+lipo -archs Ablegram_darwin_universal
+
+# move for app packaging
+mv Ablegram_darwin_* dist/os/darwin
+
+# prepare app structure
+mkdir -p dist/os/darwin/Ablegram.app/Contents/{MacOS,Resources}
+
+# place the universal binary
+cp dist/os/darwin/Ablegram_darwin_universal dist/os/darwin/Ablegram.app/Contents/MacOS/Ablegram
+
+# build and place the app icon
+mkdir -p dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset
+cp assets/icon.png dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset/icon_512x512.png
+
+for size1 in 16 32 48 128 256 512; do
+  # 1x variant
+  convert assets/icon.png -resize ${size1}x${size1}\! dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset/icon_${size1}x${size1}.png
+
+  # 2x variant
+  size2=$((2*$size1))
+  convert assets/icon.png -resize ${size2}x${size2}\! dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset/icon_${size1}x${size1}@2x.png
+done
+
+iconutil -c icns dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset -o dist/os/darwin/Ablegram.app/Contents/Resources/icon.icns
+rm -rf dist/os/darwin/Ablegram.app/Contents/Resources/icon.iconset
+
+# build and place the plist
+cat <<EOF > dist/os/darwin/Ablegram.app/Contents/Info.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleName</key>
+	<string>Ablegram</string>
+	<key>CFBundleExecutable</key>
+	<string>ablegram</string>
+	<key>CFBundleIdentifier</key>
+	<string>app.ablegram.ablegram</string>
+	<key>CFBundleIconFile</key>
+	<string>icon.icns</string>
+	<key>CFBundleShortVersionString</key>
+	<string>${APP_VERSION}</string>
+	<key>CFBundleSupportedPlatforms</key>
+	<array>
+		<string>MacOSX</string>
+	</array>
+	<key>CFBundleVersion</key>
+	<string>${APP_BUILD}</string>
+	<key>NSHighResolutionCapable</key>
+	<true/>
+	<key>NSSupportsAutomaticGraphicsSwitching</key>
+	<true/>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundlePackageType</key>
+	<string>APPL</string>
+	<key>LSApplicationCategoryType</key>
+	<string>public.app-category.</string>
+	<key>LSMinimumSystemVersion</key>
+	<string>10.11</string>
+</dict>
+</plist>
+EOF
+
+# sign the app
+codesign \
+  --force \
+  --options runtime \
+  --deep \
+  --sign "Developer ID Application: Adrian Rudnik (2TVJ7N94TN)" \
+  -i "app.ablegram.ablegram" \
+  dist/os/darwin/Ablegram.app
+
+# compress deployable
+tar -czvf dist/deploy/Ablegram-v${APP_VERSION}-macOS_Universal.tar.gz -C dist/os/darwin Ablegram.app

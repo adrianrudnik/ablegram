588ce66ae5181fe4aea6e0002d085d6e0e8cfd73
Adrian Rudnik <adrian@klonmaschine.de>
2023-11-04 13:46:57 +0100

    Added nore for windows specific paths

--- service/internal/collector/paths_windows.go
+++ service/internal/collector/paths_windows.go
@@ -2,5 +2,7 @@
 
 package collector
 
+// Some important paths that can contain stuff in Windows:
+// - ./AppData/Local/Ableton/Cache/PackDownloads/Upright Piano by Spitfire Audio_r58427_v1.1.alp
 func enrichPlatformExcludePaths() {
 }

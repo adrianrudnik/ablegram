8d5287e0f3ff4ce79c6de824f75a20292845d616
Adrian Rudnik <adrian@klonmaschine.de>
2023-11-12 23:30:05 +0100

    Possible variant for macOS 10.12+ that does not rely on lipo to combine different ARCHs

---
+++ service/bin/compile-macos-legacy.sh
@@ -0,0 +1,113 @@
+#!/bin/zsh -e
+
+# Requirements:
+# - brew install imagemagick
+
+# Notes:
+# - Binary must be lowercase, codesign will fail when app is extracted from dmg with message "can't be opened"
+# - Signing with --deep not good https://developer.apple.com/forums/thread/129980
+
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  APP_VERSION=$1
+else
+  echo 'Please provide numeric semver.'
+  exit 1
+fi
+
+if [[ $(uname -m) != 'arm64' ]]; then
+  echo "Use the MAC you bought."
+  exit 1
+fi
+
+SCRIPT_DIR=${0:a:h}
+cd ${SCRIPT_DIR}/..
+
+# Ensure the frontend is correctly built
+source $NVM_DIR/nvm.sh;
+rm -rf internal/webservice/.frontend
+cd ../frontend
+nvm use
+npm install
+npm run build
+mv dist ../service/internal/webservice/.frontend
+cd ../service
+
+# Enable tracing mode for easier review
+set -ex
+
+rm -rf dist/os/darwin-legacy
+mkdir -p dist/{deploy,os/darwin-legacy}
+
+# Load personal settings
+source ${SCRIPT_DIR}/settings.sh
+
+APP_ID="app.ablegram.ablegram"
+BUILD_NUMBER=$(git rev-list --count HEAD)
+BUILD_DATE=$(date +%s)
+BUILD_COMMIT=$(git rev-parse --short HEAD)
+
+# Ensure the bundled resources are up to date
+go generate
+
+CGO_ENABLED=1 \
+CGO_CFLAGS="-mmacosx-version-min=10.12" \
+CGO_LDFLAGS="-mmacosx-version-min=10.12" \
+GOOS=darwin \
+  go build \
+    -ldflags="-s -w -X=main.AppVersion=${APP_VERSION} -X=main.BuildCommit=${BUILD_COMMIT} -X=main.BuildDate=${BUILD_DATE} -X=main.BuildNumber=${BUILD_NUMBER}" \
+    -o ablegram_darwin_legacy \
+    .
+
+# Move for app packaging
+mv ablegram_darwin_* dist/os/darwin-legacy
+
+# Prepare app structure
+mkdir -p dist/os/darwin-legacy/Ablegram.app/Contents/{MacOS,Resources}
+
+# Place the universal binary
+cp dist/os/darwin-legacy/ablegram_darwin_legacy dist/os/darwin-legacy/Ablegram.app/Contents/MacOS/ablegram
+
+# Build and place the app icon
+mkdir -p dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset
+cp assets/icon.png dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset/icon_512x512.png
+
+for size1 in 16 32 48 128 256 512; do
+  # 1x variant
+  convert assets/icon.png -resize ${size1}x${size1}\! dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset/icon_${size1}x${size1}.png
+
+  # 2x variant
+  size2=$((2*$size1))
+  convert assets/icon.png -resize ${size2}x${size2}\! dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset/icon_${size1}x${size1}@2x.png
+done
+
+iconutil -c icns dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset -o dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.icns
+rm -rf dist/os/darwin-legacy/Ablegram.app/Contents/Resources/icon.iconset
+
+# Build and place the plist
+export APP_ID APP_VERSION BUILD_DATE
+envsubst '${APP_ID} ${APP_VERSION} ${BUILD_DATE}' < os/macOS/plist-template.xml > dist/os/darwin-legacy/Ablegram.app/Contents/Info.plist
+
+# Remove extended attributes
+xattr -cr dist/os/darwin-legacy/Ablegram.app
+
+# Review extended attributes
+xattr -lr dist/os/darwin-legacy/Ablegram.app
+
+# Sign the app
+codesign --force --options runtime --timestamp --sign "${SIGN_CERT}" -i "${APP_ID}" dist/os/darwin-legacy/Ablegram.app
+
+# Verify the signature
+codesign --verify --verbose dist/os/darwin-legacy/Ablegram.app
+
+# Package a signed dmg
+hdiutil create -ov -volname Ablegram -format UDZO -srcfolder dist/os/darwin-legacy/Ablegram.app -o "dist/deploy/Ablegram-v${APP_VERSION}-macOS_legacy.dmg"
+codesign --verify --verbose --sign ${SIGN_CERT} -i "${APP_ID}" "dist/deploy/Ablegram-v${APP_VERSION}-macOS_legacy.dmg"
+
+# Review the entitlements
+codesign --display --entitlements - dist/os/darwin-legacy/Ablegram.app
+
+# Submit the dmg for notarization
+xcrun notarytool submit --apple-id "${APPLE_ID}" --password "${APPLE_APP_PASSWORD}" --team-id="${TEAM_ID}" --wait "dist/deploy/Ablegram-v${APP_VERSION}-macOS_legacy.dmg"
+
+# Validate the local dmg again for notarization
+spctl -a -t open --context context:primary-signature -v "dist/deploy/Ablegram-v${APP_VERSION}-macOS_legacy.dmg"

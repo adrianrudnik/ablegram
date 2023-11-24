8934a866a907a52e4773cdda8d06930d5b9a741f
Adrian Rudnik <adrian@klonmaschine.de>
2023-11-24 12:05:29 +0100

    Small code improvements to SchemaOrg component on website

--- website/components/SchemaOrg.vue
+++ website/components/SchemaOrg.vue
@@ -3,15 +3,13 @@ import {h} from "vue";
 
 export default {
   setup(props, {slots}) {
-    if (!slots || !slots.default) {
+    if (!slots?.default) {
       return () => null;
     }
 
-    const content = slots.default();
-
     return () =>
         h("script", {
-          innerHTML: content.map(el => el.children).join(''),
+          innerHTML: slots.default?.().map(el => el.children).join(''),
           type: "application/ld+json",
         });
   },

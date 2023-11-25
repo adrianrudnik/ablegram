ef9a3849e1aa662a477cdd443a9794f218dbf99f
Adrian Rudnik <adrian@klonmaschine.de>
2023-11-25 14:30:37 +0100

    Added precompressed assets for the website

--- website/bin/build-and-publish.sh
+++ website/bin/build-and-publish.sh
@@ -2,6 +2,13 @@
 
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 
+rm -rf ./docs/.vitepress/dist
+
 npm run docs:build
+
+find ./docs/.vitepress/dist -type f \
+    -regex ".*\.\(css\|html\|js\|json\|svg\|xml\)$" \
+    -exec brotli --best {} \+ \
+    -exec gzip --best -k {} \+
 
 rsync -avh --delete ./docs/.vitepress/dist/ chinchilla-prod:/home/deploy/docker/ablegram.app/webroot
